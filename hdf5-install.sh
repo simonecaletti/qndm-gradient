e3359bd59e6f7030e3e2161e614a3d7619723af6
simonecaletti <iscrizioni.simone@gmail.com>
2024-03-07 18:21:20 +0100

    Added hdf5-install script

---
+++ hdf5-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo "Installing the hsf5 lybrary:"
+
+#creating a new env with conda
+conda create -n qndm python=3.9.13
+conda activate qndm
+
+#Networkx package
+conda install -c anaconda networkx
+
+#mat2qubit dependencies
+conda install -c conda-forge json5
+
+#Mat2qubit package
+git clone https://github.com/IntelLabs/mat2qubit.git
+cd
+
+#
