149dd0f51cc3b62551f1deb724dc5190b0c68463
giovanniminuto <98158370+giovanniminuto@users.noreply.github.com>
2024-04-26 11:27:28 +0200

    Update hdf5-install.sh

--- hdf5-install.sh
+++ hdf5-install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
+# Exit immediately if a command exits with a non-zero status,
+# and treat unset variables as errors
+set -euo pipefail
+
 echo "Installing the hsf5 library:"
 
+# Activate conda (assuming conda is installed and configured)
+# This assumes conda is in your PATH
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
 #creating a new env with conda
-conda create -n qndm python=3.9.13
-conda activate qndm
+conda create -n qndm2 python=3.9.13
+conda activate qndm2
 
 #Networkx package
 conda install -c anaconda networkx
@@ -14,6 +22,7 @@ conda install -c conda-forge json5
 
 #Mat2qubit package
 git clone https://github.com/IntelLabs/mat2qubit.git
-cd
 
-#
+pip install -r requirements.txt
+
+cd
